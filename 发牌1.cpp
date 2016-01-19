5724a3c7b59fe022e5333506af64e84bf32fe922
L2016J <1642487260@qq.com>
2016-01-19 18:57:55 +0800

    加注释

--- 发牌1.cpp
+++ 发牌1.cpp
@@ -7,7 +7,7 @@ struct puke
 	char color;
 	char num;
 };//每张牌的花色和数字
-void m();
+void m();//发牌函数 
 int main()
 {
 	char ch;
@@ -18,19 +18,20 @@ int main()
 		cin>>ch; 
 	}while(ch!='0');
 		
- }                                 
+}                                 
 void m()
 {
+///////////////////////////////////////////////////////	
+///////////用来给结构体数组pk[54]赋值//////////////////	
 	srand((unsigned)time(NULL));    //产生随机数的种子 
 	int i,j,t=1,ss,tt=0,person=1;
-	char co='A',nn;
+	char co=6,nn;                   //co=6,ascll码为6的字符模拟“黑桃” 
     struct puke pk[55];             //1-54张牌 
     for(i=0;i<4;i++)                //用双重for来给pk[]赋值(大王小王单独赋值） 
-    {
+    {                                
     	for(j=0;j<=12;j++)
     	{
-		    nn='1';
-    		pk[t].color=co;
+		    nn='1';        
     		if(j==9)
     		nn='+';
     		else if(j==10)
@@ -40,17 +41,20 @@ void m()
     		else if(j==12)
     		nn='K';
     		else
-    		nn=nn+j;             
-    		pk[t].num=nn;
+    		nn=nn+j;              
+			pk[t].color=co;       //给花色赋值          
+    		pk[t].num=nn;         //给点数赋值 
     		t++;
 		}
-		co++;
+		co--;                     //改变当前花色 
 	}
 	pk[t].color='X';
 	pk[t++].num='W';
 	pk[t].color='D';
 	pk[t].num='W';
 	cout<<endl;
+///////////////////////////////////////////////////////	
+///////用来输出54张牌及格式控制////////////////////////	
 	while(tt!=54)
 	{
 	
@@ -66,13 +70,12 @@ void m()
 			}
 			cout<<pk[ss].color<<pk[ss].num<<"  "; //"发牌" 
 			pk[ss].color='N';                     //标记"发过"的牌 
-			tt++;
+			tt++;                                 //计数器，记已发过的牌数     
 		}
 		if(tt%12==0)                      //每个人12张牌 
 		{
 			cout<<endl;
 		}
 	}
-	cout<<endl;
-		
+	cout<<endl;		
 } 
--- 发牌2.cpp
+++ 发牌2.cpp
@@ -3,24 +3,29 @@
 #include<stdlib.h>
 #include<string.h>
 using namespace std;
-void m();
-char *Print(int n)
-{
-	int i,j;
+//用来形成一张牌，用字符数组装数据（ch[0]->花色，ch[1]->点数）  
+//n是范围是1-54的随机数 
+//返回字符指针 
+char *Print(int n);
+//发牌函数
+void m(); 
+
+char *Print(int n)        //分解n构成ch[0],ch[1] 
+{                  
+	int Huase,Dian;
 	char ch[10],c;
-	i=n/13+1;
-	j=n%13;
-	
-	if(j==10)
+	Huase=n/13+1;    //其范围是1-4，代表花色 
+	Dian=n%13;       //其范围是0-12，代表点数     
+	if(Dian==10)     //点数是10,11,12,13的要特殊考虑 
     c='+';
-    else if(j==11)
+    else if(Dian==11)
     c='J';
-    else if(j==12)
+    else if(Dian==12)
     c='Q';
-    else if(j==13)
+    else if(Dian==0)
     c='K';
     else 
-    c=char (j+'0');
+    c=char (Dian+'0');  //实现数字n->字符n（n范围是1-9） 
     
 
 	if(n==53)
@@ -33,27 +38,27 @@ char *Print(int n)
 		ch[0]='D';
 		ch[1]='W';
 	}
-	else if(i==1)
-	{
-		ch[0]=3;
-		ch[1]=c;
-	}
-	else if(i==2)
+	else if(Huase==1)  //黑桃 
 	{
 		ch[0]=6;
 		ch[1]=c;
 	}
-	else if(i==3)
+	else if(Huase==2)  //红桃 
 	{
 		ch[0]=5;
 		ch[1]=c;
 	}
-	else if(i==4)
+	else if(Huase==3)  //棉花 
 	{
-	    ch[0]=4;
+		ch[0]=4;
 		ch[1]=c;
 	}
-	ch[2]='\0';
+	else if(Huase==4)  //方块 
+	{
+	    ch[0]=3;
+		ch[1]=c;
+	}
+	ch[2]='\0';        //字符串结束符号 
 	return ch;
 	
 }
@@ -70,16 +75,16 @@ int main()
 void m()
 {
 	int i,j,k,t=0,ss=1;
-	int vis[55]={0};
+	int vis[55]={0};       //用vis[n]=0代表，第n张牌已经发过 
 	char *ch;
 	srand((unsigned)time(NULL));
-	while(t!=54)
+	while(t!=54)           //发54张牌 
 	{
 	
 		k=rand()%54+1;
 		if(vis[k]==0)
 		{
-			ch=Print(k);
+			ch=Print(k);        //构造牌的数据 
 			if(t%12==0&&t<48)
 			{
 			cout<<ss<<':';
@@ -87,10 +92,9 @@ void m()
 		    }
 			cout<<ch<<" ";
 			vis[k]=1;
-			t++;
-			if(t%12==0&&t!=0)
-			cout<<endl; 
-			
+			t++;             //计数器，记已发的牌数 
+			if(t%12==0&&t!=0) 
+			cout<<endl; 			
 		}
 		else
 		continue; 
